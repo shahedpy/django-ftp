35ec578ce632d48521948c313d3e85cfe54e6d9c
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2025-12-04 11:51:59 +0600

    file system

--- myftp/settings.py
+++ myftp/settings.py
@@ -110,12 +110,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Change FTP server filesystem
-FTPSERVER_FILESYSTEM = 'django_ftpserver.filesystems.StorageFS'
-# Using Amazon S3 storage (django-storages)
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# Config for S3Boto3Storage
-AWS_ACCESS_KEY_ID = '(your access key id)'
-AWS_SECRET_ACCESS_KEY = 'your secret access key'
-AWS_STORAGE_BUCKET_NAME = 'your.storage.bucket'
