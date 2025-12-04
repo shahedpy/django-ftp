6be961a2b49b09703721bb6cdd14c87f47a8cb39
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2025-12-04 12:44:17 +0600

    ftp directory

--- CONFIG/settings.py
+++ CONFIG/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -113,3 +114,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FTPSERVER_DIRECTORY = os.path.join(BASE_DIR, 'media',)
